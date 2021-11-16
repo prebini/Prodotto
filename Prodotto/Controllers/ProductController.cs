42170c258dfd1d322691d2f2c93e0ec71fd8d213
david <david@LAPTOP-D21E46F3>
2021-11-17 00:22:46 +0100

    Aggiungere i file di progetto.

---
+++ Prodotto/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prodotto.EF.Data;
+using Prodotto.EF.Entity;
+using Prodotto.Models;
+using Prodotto.Utile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodotto.Controllers
+{
+    public class ProductController : Controller
+    {
+        private NORTHWINDContext _context;
+        
+
+        public ProductController(NORTHWINDContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Insert(ProductViewModel element)
+        {
+             if (_context.Products.Count() != 0)
+            {
+                element.ProductId = _context.Products.Max(x => x.ProductId) + 1;
+            }
+       else
+            {
+                element.ProductId = 1;
+            }
+            _context.Products.Add(element.ProjectToDbModel());// aggiunge il prodotto element nella tab products
+            _context.SaveChanges();
+            return View();
+        }
+      
+
+        public IActionResult Read()
+        {
+            return View();
+        }
+
+        public IActionResult Update(ProductViewModel element)
+        {
+            _context.Products.Update(element.ProjectFromViewModel()); //utilizzo metodo aggiorna del dbcontext
+            _context.SaveChanges(); // salvo l'aggiornamento 
+            return View();
+        }
+
+        public IActionResult Delete(ProductViewModel element)
+        {
+            _context.Remove(element);
+            _context.SaveChanges();
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            List<ProductViewModel> lista = _context.Products.AsEnumerable().ProjectToViewModel().ToList();
+            return View(lista);
+
+
+        }
+    }
+}
+
+         
---
+++ Prodotto/Utile/Prog.cs
@@ -0,0 +1,53 @@
+﻿using Prodotto.EF.Entity;
+using Prodotto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodotto.Utile
+{
+    public static class Prog
+    {
+        internal static Products ProjectToDbModel(this ProductViewModel prodotto)
+        {
+            return new Products()
+            {
+                ProductId = prodotto.ProductId,
+                ProductName = prodotto.ProductName,
+                CategoryId = prodotto.CategoryId,
+                UnitPrice = prodotto.UnitPrice,
+                UnitsInStock = prodotto.UnitsInStock,
+                Discontinued = prodotto.Discontinued,
+                QuantityPerUnit = prodotto.QuantityPerUnit,
+            };
+        }
+        internal static IEnumerable<ProductViewModel> ProjectToViewModel(this IEnumerable<Products> query)
+        {
+            return query.Select(x => new ProductViewModel()
+            {
+                ProductId = x.ProductId,
+                ProductName = x.ProductName,
+                CategoryId = x.CategoryId,
+                UnitPrice = x.UnitPrice,
+                UnitsInStock = x.UnitsInStock,
+                Discontinued = x.Discontinued,
+                QuantityPerUnit = x.QuantityPerUnit,
+            });
+        }
+        internal static Products ProjectFromViewModel(this ProductViewModel prodotto)
+        {
+            return new Products()
+            {
+                ProductId = prodotto.ProductId,
+                ProductName = prodotto.ProductName,
+                CategoryId = prodotto.CategoryId,
+                UnitPrice = prodotto.UnitPrice,
+                UnitsInStock = prodotto.UnitsInStock,
+                Discontinued = prodotto.Discontinued,
+                QuantityPerUnit = prodotto.QuantityPerUnit,
+            };
+        }
+
+    }
+}
